aafb4bb7acba370226ba06362bf36862a9356e80
Madinkaa <56644215+Madinkaa@users.noreply.github.com>
2021-03-30 22:59:52 +0600

    Add files via upload

---
+++ lab7/2e.py
@@ -0,0 +1,8 @@
+n = int(input())
+k = int(input())
+if n > k:
+    print(1)
+if n == k:
+    print(0)
+else:
+    print(2)
---
+++ lab7/3a.py
@@ -0,0 +1,5 @@
+a = int(input())
+n = int(input())
+for i in range(a, n+1):
+    if(i%2 == 0):
+        print(i, end = ' ')
